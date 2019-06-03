bcb9be8ebfca2b80b52ad2abedf74f955026285c
samarth <samarth29@github.com>
2019-06-02 18:03:50 -0700

    initial commit and added dependencies

---
+++ src/main/java/com/samarth/app/es/SpringBootElasticsearchUsingTransportClientApplication.java
@@ -0,0 +1,13 @@
+package com.samarth.app.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElasticsearchUsingTransportClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootElasticsearchUsingTransportClientApplication.class, args);
+	}
+
+}
